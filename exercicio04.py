a4e46a06a665be7ae59d57eddb51dd63b4c99793
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-05-29 12:20:08 +0000

    exercicioo4.py

--- exercicio04.py
+++ exercicio04.py
@@ -2,3 +2,15 @@
 # O programa deve receber a distância percorrida e o tempo gasto, calcular a velocidade e indicar se foi 
 # lenta (<5 km/h), moderada (5 a 10 km/h) ou rápida (>10 km/h).
 
+
+peso = float(input("digite o peso (kg):"))
+altura = float(input("digite a altura(m):"))
+
+imc = peso / (altura**2)
+
+  print(f"IMC:{imc:.2f}")
+
+ if imc < 18.5:
+   print("Abaixo do peso.")
+   
+      
