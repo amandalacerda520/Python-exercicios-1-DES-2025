49723416915d08ab2e70d24f47934429b4948133
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-06-03 13:43:57 +0000

    exercicio04.py terminado

--- exercicio02.py
+++ exercicio02.py
@@ -2,4 +2,12 @@
 # para finalizar três tarefas: X, Y e Z.
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
+dia01 = int(input("digite o tempo nessecario para entregar a atividade x "))
+dia02 = int(input("Digite o tempo necessário para entregar a atividade y")) 
+dia03 = int(input("Digite o tempo  necessário para entregar a atividade z"))
 
+if dia01 < 0 or dia02 < 0 or dia03 < 0:
+    print("erro negativo.")
+else:
+    soma = dia01 + dia02 + dia03
+    print(f"tempo total da atividade: {soma} dias ")
--- exercicio04.py
+++ exercicio04.py
@@ -1,21 +1,13 @@
 # Isabela está desenvolvendo um aplicativo de corrida que calcula a velocidade média do usuário.
 # O programa deve receber a distância percorrida e o tempo gasto, calcular a velocidade e indicar se foi 
 # lenta (<5 km/h), moderada (5 a 10 km/h) ou rápida (>10 km/h).
+distancia = float(input("digite a distancia"))
+tempo = float(input("digite o tempo"))
+v_media = distancia / tempo 
 
-
-peso = float(input("digite o peso (kg):"))
-altura = float(input("digite a altura(m):"))
-
-imc = peso / (altura**2)
-
-  print(f"IMC:{imc:.2f}")
-
- if imc < 18.5:
-   print("Abaixo do peso.")
-elif imc < 25:
-   print ("Peso normal.")
+if v_media < 5:
+    print("lento")
+elif v_media >= 5 <=10:
+    print("moderado")
 else:
-   print ("acima do peso.")
-
-   
-      
+    print("rapido")
