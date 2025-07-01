630e21b3f34cf838414ed18040aa591068c88cbf
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 13:34:10 +0000

    exercicio18.py terminado

--- exercicio18.py
+++ exercicio18.py
@@ -2,3 +2,27 @@
 #Fórmula: (nota1 × peso1 + nota2 × peso2) / (peso1 + peso2)
 
 #Exercício 19 – Números e
+
+# Solicita o salário atual do usuário
+salario_atual = float(input("Digite o seu salário atual em R$: "))
+
+# Calcula o reajuste e o novo salário
+if salario_atual <= 2000.00:
+    percentual_reajuste = 0.15
+    reajuste = salario_atual * percentual_reajuste
+    novo_salario = salario_atual + reajuste
+    print(f"\nVocê terá um reajuste de 15%!")
+elif 2000.01 <= salario_atual <= 5000.00:
+    percentual_reajuste = 0.10
+    reajuste = salario_atual * percentual_reajuste
+    novo_salario = salario_atual + reajuste
+    print(f"\nVocê terá um reajuste de 10%!")
+else:  
+    percentual_reajuste = 0.05
+    reajuste = salario_atual * percentual_reajuste
+    novo_salario = salario_atual + reajuste
+    print(f"\nVocê terá um reajuste de 5%!")
+
+# Exibe os resultados
+print(f"Valor do reajuste: R$ {reajuste:.2f}")
+print(f"Seu novo salário será: R$ {novo_salario:.2f}")
