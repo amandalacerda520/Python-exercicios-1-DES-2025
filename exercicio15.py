deea736d609b98321b29ae8b01816dc09c53fc38
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 12:09:09 +0000

    exercicio15.py terminado

--- exercicio15.py
+++ exercicio15.py
@@ -2,3 +2,10 @@
 
 #Permitido: 13 anos ou mais
 
+idade = int(input("Por favor, digite sua idade: "))
+
+
+if idade >= 13:
+    print("\nVocê pode se cadastrar no site! Bem-vindo(a)!")
+else:
+    print("\nDesculpe, você não tem idade suficiente para se cadastrar neste site.")
