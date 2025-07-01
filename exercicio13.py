f0dc57735c73ce3d0f829c5d200565f994c592e9
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 11:48:46 +0000

    exercicio12.py terminado

--- exercicio13.py
+++ exercicio13.py
@@ -1,8 +1,9 @@
  #Crie um programa que receba um ano e diga se ele é bissexto.
 #(Dica: múltiplos de 4, exceto os múltiplos de 100, a menos que também sejam múltiplos de 400)
 
-ano = int(input("digite um ano: "))
+ano = int(input("Digite o ano: "))
 
-if (ano % 4 == 0 and % 100 ! = 0) or (ano % 400 == 0): 
-    print(f"{ano} é um ano bissexto.")
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano {ano} é bissexto.")
 else:
+    print(f"O ano {ano} não é bissexto.")
