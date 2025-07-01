d3525eaa7cab844b1fa6242176addea0a2616981
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 11:58:49 +0000

    exercicio11.py termindo

--- exercicio11.py
+++ exercicio11.py
@@ -5,7 +5,25 @@
 #Peso normal (18.5 a 24.9)
 #Sobrepeso (25 a 29.9)
 #Obesidade (>= 30)
-#Obesidade (>= 30)
+
+
+peso = float(input("Digite seu peso em kg: "))
+
+
+altura = float(input("Digite sua altura em metros: "))
+
+imc = peso / (altura ** 2)
+
+print(f"\nSeu IMC é: {imc:.2f}")
+
+if imc < 18.5:
+    print("Classificação: Abaixo do peso")
+elif 18.5 <= imc <= 24.9:
+    print("Classificação: Peso normal")
+elif 25 <= imc <= 29.9:
+    print("Classificação: Sobrepeso")
+else: 
+    print("Classificação: Obesidade")
 
 
 
