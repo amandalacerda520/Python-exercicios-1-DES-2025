202da2f8f872a41298a981cc39b15b6d6918bc1c
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 11:42:30 +0000

    exercicio12.py terminado

--- exercicio12.py
+++ exercicio12.py
@@ -1,5 +1,10 @@
 #Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
 #Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
-
-
  
+senha = int(input("digite a senha"))
+
+if senha < 8:
+    print("senha valida")
+
+if senha > 8: 
+    print("senha muito curta") 
--- exercicio13.py
+++ exercicio13.py
@@ -1,3 +1,8 @@
  #Crie um programa que receba um ano e diga se ele é bissexto.
 #(Dica: múltiplos de 4, exceto os múltiplos de 100, a menos que também sejam múltiplos de 400)
 
+ano = int(input("digite um ano: "))
+
+if (ano % 4 == 0 and % 100 ! = 0) or (ano % 400 == 0): 
+    print(f"{ano} é um ano bissexto.")
+else:
