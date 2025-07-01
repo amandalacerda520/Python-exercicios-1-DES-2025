b1b078c452d7c525c4ef3cfcb7760ca3849a881a
amandalacerda520 <amanda.oliveira.lacerda@escola.pr.gov.br>
2025-07-01 12:11:20 +0000

    exercicio16.py terminado

--- exercicio16.py
+++ exercicio16.py
@@ -4,3 +4,10 @@
 #De R$ 2000,01 a R$ 5000,00: +10%
 #Acima de R$ 5000,00: +5%
 
+idade = int(input("Por favor, digite sua idade: "))
+
+if idade >= 13:
+    print("\nVocê pode se cadastrar no site! Bem-vindo(a)!")
+else:
+    print("\nDesculpe, você não tem idade suficiente para se cadastrar neste site.")
+
